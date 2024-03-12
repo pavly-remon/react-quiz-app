d628547da9d9c58b2c104e4d62b776c9fc6a1791
Pavly Remon <pavlyremon@gmail.com>
2024-03-12 20:43:45 +0000

    Fix Timer Issue

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,22 +1,58 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Timer from "./Timer";
 
-const TIMER = 10000;
-const Question = ({ question, handleSelectAnswer, correctAnswer }) => {
-  const shuffled = [...question.answers].sort(() => Math.random() - 0.5);
+const Question = ({ question, handleSelectAnswer }) => {
+  const shuffled = useRef();
+  const [showAnswer, setShowAnswer] = useState(false);
+  const [selected, setSelected] = useState(-1);
+  let timer = 10000;
+  if (selected !== -1) {
+    timer = 3000;
+  }
+  if (!shuffled.current) {
+    shuffled.current = [...question.answers].sort(() => Math.random() - 0.5);
+  }
+  const correctAnswerIndex = shuffled.current.indexOf(question.answers[0]);
   const onTimeout = () => {
     handleSelectAnswer(null);
   };
+  const checkAnswer = (answer, index) => {
+    setSelected(index);
+    setTimeout(() => {
+      setShowAnswer(true);
+      setTimeout(() => {
+        handleSelectAnswer(answer);
+      }, 2000);
+    }, 1000);
+  };
   return (
     <div id="question">
-      <Timer key={question.id} timeout={TIMER} onTimeout={onTimeout} />
+      <Timer
+        key={timer}
+        timeout={timer}
+        onTimeout={selected === -1 ? onTimeout : null}
+      />
       <h2>{question.text}</h2>
       <ul id="answers">
-        {shuffled.map((answer, index) => (
-          <li key={index} className="answer">
-            <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
-          </li>
-        ))}
+        {shuffled.current.map((answer, index) => {
+          let cssClass = "";
+          if (selected === index) {
+            cssClass = "selected";
+            if (showAnswer) {
+              cssClass += index === correctAnswerIndex ? " correct" : " wrong";
+            }
+          }
+          return (
+            <li key={index} className="answer">
+              <button
+                onClick={() => checkAnswer(answer, index)}
+                className={cssClass}
+              >
+                {answer}
+              </button>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -2,21 +2,19 @@ import { useState, useEffect } from "react";
 const Timer = ({ timeout, onTimeout }) => {
   const [time, setTime] = useState(timeout);
   useEffect(() => {
-    console.log("interval");
     const timer = setInterval(() => {
-      setTime((prev) => prev - 100);
-    }, 100);
+      setTime((prev) => prev - 10);
+    }, 10);
     return () => {
       clearInterval(timer);
     };
   }, []);
   useEffect(() => {
-    console.log("timeout");
     const timer = setTimeout(onTimeout, timeout);
     return () => {
       clearTimeout(timer);
     };
-  }, []);
+  }, [timeout]);
   return <progress value={time} max={timeout} />;
 };
 
